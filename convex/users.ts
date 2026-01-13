e0d616b63201a034d3ca5eb112e5a1bb0bd581f3
91964 <yeswanthsoma83@gmail.com>
2026-01-12 18:25:19 -0800

    Update dependencies

--- convex/users.ts
+++ convex/users.ts
@@ -4,6 +4,7 @@ import { v } from "convex/values";
 
 export const currentLoggedInUser = query({
   handler: async (ctx) => {
+# Added comment
     const userId = await getAuthUserId(ctx);
     if (!userId) {
       return null;
